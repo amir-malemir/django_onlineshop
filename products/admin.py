2cd3ab8feeefac6d4be05e71ee214bc0388e85f4
amir-malemir <amir.maalehmir@gmail.com>
2024-06-16 12:01:34 +0330

    add verbose name

--- products/admin.py
+++ products/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from .models import Products, Comments
 
 
+class CommentsInline(admin.TabularInline):
+    model = Comments
+    fields = ('author', 'body', 'stars', 'active')
+    extra = 1
+
+
 @admin.register(Products)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'price', 'active', 'datetime_created', 'datetime_modified')
+    inlines = [CommentsInline]
 
 
 @admin.register(Comments)
--- products/models.py
+++ products/models.py
@@ -21,6 +21,11 @@ class Products(models.Model):
         return reverse('product_detail', args=[self.pk])
 
 
+class ActiveCommentsManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveCommentsManager, self).get_queryset().filter(active=True)
+
+
 class Comments(models.Model):
     PRODUCT_STARS = [
         ('1', 'Very Bad'),
@@ -31,13 +36,17 @@ class Comments(models.Model):
     ]
     product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='comments',)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments', )
-    body = models.TextField()
-    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
+    body = models.TextField(verbose_name='your comment: ')
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name='rate')
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
     active = models.BooleanField(default=True)
+
+    # Manager
+    objects = models.Manager()
+    Active_comments_manager = ActiveCommentsManager()
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.product.id])
