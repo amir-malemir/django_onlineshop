587843cea51c67e61acddae72217914e6c108316
amir-malemir <amir.maalehmir@gmail.com>
2024-06-29 13:36:05 +0330

    add del products from cart

--- cart/cart.py
+++ cart/cart.py
@@ -29,7 +29,13 @@ class Cart:
             self.cart[product_id]['quantity'] += quantity
         self.save()
 
+    def remove(self, product):
+        """ remove a product from the cart """
+        product_id = str(product.id)
 
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
 
     def save(self):
         """ mark session as modified to save changes """
