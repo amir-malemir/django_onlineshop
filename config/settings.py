38cfdc09d3872f8d84d511d9dd71181b04f177fc
amir-malemir <amir.maalehmir@gmail.com>
2024-07-12 12:53:28 +0330

    include cart sidebar

--- config/settings.py
+++ config/settings.py
@@ -79,6 +79,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.request',
+                # custom context processors
+                'cart.context_processors.cart',
             ],
         },
     },
