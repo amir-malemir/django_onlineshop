44cd13b6dd554104b25ea70419d55387b4ed30a3
amir-malemir <amir.maalehmir@gmail.com>
2024-07-20 13:43:25 +0330

    add urls path of orders url

--- config/urls.py
+++ config/urls.py
@@ -26,8 +26,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
-
-
+    path('order/', include('orders.urls')),
 
 # rosetta
     path('rosetta/', include('rosetta.urls')),
