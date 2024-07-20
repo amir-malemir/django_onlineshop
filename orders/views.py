24c24a242b61e107912928d7e0dda3b3b53b5906
amir-malemir <amir.maalehmir@gmail.com>
2024-07-20 15:43:20 +0330

    add form in order detail with custom fileds and translate filed names

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
-
+from .forms import OrderForm
 def order_create_view(request):
-    return render(request, 'order/order_create.html')
+    return render(request, 'order/order_create.html', context={
+        'form': OrderForm()
+    })
